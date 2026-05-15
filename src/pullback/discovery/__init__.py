889237f4c2778f28e0f53c93910825f591160fbb
dsleo <leooleds@gmail.com>
2026-05-15 16:49:24 +0200

    chore: make heavy deps optional at import time

--- src/pullback/discovery/__init__.py
+++ src/pullback/discovery/__init__.py
@@ -1,12 +1,53 @@
 from .arxiv.ids import extract_arxiv_id_from_text, normalize_arxiv_id
-from .arxiv.metadata import PaperMetadata, PaperMetadataFetcher, fetch_arxiv_metadata
 from .base import DiscoveryAccessError, PaperDiscoveryClient
-from .pipeline import ChainedDiscoveryClient
-from .providers.arxiv_api import ArxivAPIDiscoveryClient
-from .providers.semantic_scholar import SemanticScholarDiscoveryClient
-from .providers.openalex import OpenAlexDiscoveryClient
-from .providers.openrouter_search import OpenRouterSearchDiscoveryClient
-from .providers.zbmath_open import ZbMathOpenDiscoveryClient
+
+try:
+    from .providers.arxiv_api import ArxivAPIDiscoveryClient
+except ModuleNotFoundError:  # pragma: no cover
+    ArxivAPIDiscoveryClient = None  # type: ignore
+
+try:
+    from .providers.arxiv_search_html import ArxivSearchHtmlDiscoveryClient
+except ModuleNotFoundError:  # pragma: no cover
+    ArxivSearchHtmlDiscoveryClient = None  # type: ignore
+
+try:
+    from .providers.semantic_scholar import SemanticScholarDiscoveryClient
+except ModuleNotFoundError:  # pragma: no cover
+    SemanticScholarDiscoveryClient = None  # type: ignore
+
+try:
+    from .providers.openalex import OpenAlexDiscoveryClient
+except ModuleNotFoundError:  # pragma: no cover
+    OpenAlexDiscoveryClient = None  # type: ignore
+
+try:
+    from .providers.openrouter_search import OpenRouterSearchDiscoveryClient
+except ModuleNotFoundError:  # pragma: no cover
+    OpenRouterSearchDiscoveryClient = None  # type: ignore
+
+try:
+    from .providers.web_search_arxiv import WebSearchArxivDiscoveryClient, WebSearchArxivConfig
+except ModuleNotFoundError:  # pragma: no cover
+    WebSearchArxivDiscoveryClient = None  # type: ignore
+    WebSearchArxivConfig = None  # type: ignore
+
+try:
+    from .providers.zbmath_open import ZbMathOpenDiscoveryClient
+except ModuleNotFoundError:  # pragma: no cover
+    ZbMathOpenDiscoveryClient = None  # type: ignore
+
+try:  # Optional dependency: limiter + fallback modules
+    from .pipeline import ChainedDiscoveryClient
+except ModuleNotFoundError:  # pragma: no cover
+    ChainedDiscoveryClient = None  # type: ignore
+
+try:  # Optional dependency: `arxiv` library
+    from .arxiv.metadata import PaperMetadata, PaperMetadataFetcher, fetch_arxiv_metadata
+except ModuleNotFoundError:  # pragma: no cover
+    PaperMetadata = None  # type: ignore
+    PaperMetadataFetcher = None  # type: ignore
+    fetch_arxiv_metadata = None  # type: ignore
 
 __all__ = [
     "fetch_arxiv_metadata",
@@ -18,8 +59,11 @@ __all__ = [
     "PaperDiscoveryClient",
     "ChainedDiscoveryClient",
     "ArxivAPIDiscoveryClient",
+    "ArxivSearchHtmlDiscoveryClient",
     "OpenRouterSearchDiscoveryClient",
     "OpenAlexDiscoveryClient",
     "ZbMathOpenDiscoveryClient",
     "SemanticScholarDiscoveryClient",
+    "WebSearchArxivDiscoveryClient",
+    "WebSearchArxivConfig",
 ]
